c678cc6a4afe1ad9fe9ed17802c8aa9630ae9b9c
nirmal.m0509@gmail.com <nirmal.m0509@gmail.com>
2026-07-03 12:45:19 +0000

    trip_fact model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,16 @@
+with trips as(
+    select
+    ride_id,
+    rideable_type,
+    date(to_timestamp(started_at)) as trip_date,
+    start_statio_id as start_station_id,
+    end_station_id,
+    member_csual as member_casual,
+    timestampdiff(seconds,to_timestamp(started_at),to_timestamp(ended_at)) as trip_duration_seconds
+
+    from {{ source('demo', 'bike') }}
+    where ride_id != 'ride_id'
+
+)
+
+select * from trips
